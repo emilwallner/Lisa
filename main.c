66becfd4172f41e1cfbb78ba605a360d71f4e69c
Emil Wallner <emilw@llner.co>
2016-12-12 23:03:59 +0100

    draw a line

--- main.c
+++ main.c
@@ -6,11 +6,35 @@
 /*   By: ewallner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 17:11:59 by ewallner          #+#    #+#             */
-/*   Updated: 2016/12/12 17:38:48 by ewallner         ###   ########.fr       */
+/*   Updated: 2016/12/12 23:03:28 by ewallner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
+
+
+void coord_to_line(int xi,int yi,int xf,int yf) 
+{
+	void *mlx;
+	void *win;
+	int x;
+	int y;
+	float a;
+	float b;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 400, 400, "mlx 42");
+
+	a =(float) (yf-yi)/(xf-xi) ;
+	b = yi - a * xi;
+	x = xi;
+	while(x <= xf)
+	{
+		y =(int) (a * x + b);
+		mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
+		x++;
+	}
+}
 
 int			main()
 {
@@ -19,19 +43,34 @@ int			main()
 	int x;
 	int y;
 
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 400, 400, "mlx 42");
+//	mlx = mlx_init();
+//	win = mlx_new_window(mlx, 400, 400, "mlx 42");
+
+	coord_to_line(0, -100, 0, 100);
+	/*
+	void ligne(int xi,int yi,int xf,int yf) {
+		int x,y ;
+		float a,b ;
+		a =(float) (yf-yi)/(xf-xi) ;
+		b = yi - a * xi ;
+		for ( x = xi ; x <= xf ; x++ ) {
+			y =(int) (a * x + b) ;
+			mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
+		}
+	}
+
 	y = 50;
-	while (y < 150)
-	{
-		x = 50;
-		while (x < 150)
-		{
+			while (y < 150)
+			{
+			x = 50;
+			while (x < 150)
+			{
 			mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
 			x++;
-		}
-		y++;
-	}
+			}
+			y++;
+			}
+			*/
 	mlx_loop(mlx);
 	return (0);
 }
